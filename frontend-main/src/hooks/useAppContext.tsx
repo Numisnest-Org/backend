38142cbedae09c88322b61555f8993020b870e34
Muhammad Talha Sohail <63157276+Talha-005@users.noreply.github.com>
2024-08-09 23:34:10 +0500

    histiry

--- frontend-main/src/hooks/useAppContext.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { DataValueContext } from "src/utilities/context/context";
-const useAppContext = () => {
-   return useContext(DataValueContext);
-};
-
-export default useAppContext;
